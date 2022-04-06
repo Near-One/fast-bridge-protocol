d82c69529946222f2b72359ed32a1e3218a19090
vladyslavBudiuk <vladyslav.b@blaize.tech>
2022-04-06 22:11:56 +0300

    Add tokens whitelist functional

---
+++ contracts/EthErc20FastBridge.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.11;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract EthErc20FastBridge is Ownable {
+    mapping (address => bool) public whitelistedTokens;
+
+    modifier isWhitelisted(address _token) {
+        require(whitelistedTokens[_token], "Token not whitelisted!");
+        _;
+    }
+
+    function isTokenInWhitelist(address _token) external returns(bool) {
+        return whitelistedTokens[_token];
+    }
+
+    function setTokens(
+        address[] memory _tokens,
+        bool[] memory _states
+    ) 
+        public 
+        onlyOwner 
+    {
+        require(_tokens.length == _states.length, "Arrays must be equal");
+
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            whitelistedTokens[_tokens[i]] = _states[i];
+        }
+    }   
+    
+}
