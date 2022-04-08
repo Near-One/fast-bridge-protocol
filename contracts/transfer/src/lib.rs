9fd09dad9d427b485d57c93000f8826c15dea555
Tymofii <timofii.savinov@gmail.com>
2022-04-08 12:21:40 +0300

    fix comments

--- contracts/transfer/src/lib.rs
+++ contracts/transfer/src/lib.rs
@@ -1,42 +1,63 @@
 use near_sdk::collections::{LookupMap, LookupSet};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, AccountId, log, PromiseOrValue, serde_json};
-use near_sdk::env::block_timestamp;
+use near_sdk::{near_bindgen, ext_contract, AccountId, log, PromiseOrValue, serde_json, env, Gas};
+use near_sdk::env::{block_timestamp, signer_account_id};
 use near_sdk::json_types::U128;
-use std::time::{SystemTime, UNIX_EPOCH};
 use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::env::sha256;
+use std::str;
+
 
 const LOCK_TIME_MIN: u64 = 3600;
 const LOCK_TIME_MAX: u64 = 7200;
+pub const NO_DEPOSIT: u128 = 0;
+pub const TGAS: Gas = near_sdk::Gas::ONE_TERA;
 
-#[derive(Serialize, Deserialize)]
+#[near_bindgen]
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
 struct TransferData {
-    token: String,
+    token: AccountId,
     amount: u128,
 }
 
-#[derive(Serialize, Deserialize)]
+#[near_bindgen]
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
-struct TokenMsg {
+struct TransferMessage {
     valid_till: u64,
     transfer: TransferData,
     fee: TransferData,
     recipient: String,
 }
 
+#[ext_contract(ext_token)]
+trait InternalToken {
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: u128) -> PromiseOrValue<U128>;
+}
+
+#[ext_contract(ext_self)]
+trait InternalTokenInterface {
+    fn withdraw_amount_callback(&mut self, transfer_message: TransferMessage) -> PromiseOrValue<U128>;
+    fn update_balance_callback(&mut self, transfer_message: TransferMessage) -> PromiseOrValue<U128>;
+}
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Transfer {
-    locked_accounts: LookupMap<AccountId, String>,
+    pending_transfers: LookupMap<String, LookupMap<AccountId, TransferMessage>>,
     available_tokens: LookupSet<String>,
+    user_balances: LookupMap<AccountId, LookupMap<AccountId, u128>>,
+    transactions: u64,
 }
 
 impl Default for Transfer {
     fn default() -> Self {
         Self {
-            locked_accounts: LookupMap::new(b"s".to_vec()),
-            available_tokens: LookupSet::new(b"s".to_vec()),
+            pending_transfers: LookupMap::new(b"a".to_vec()),
+            available_tokens: LookupSet::new(b"b".to_vec()),
+            user_balances: LookupMap::new(b"c".to_vec()),
+            transactions: 0,
         }
     }
 }
@@ -45,81 +66,232 @@ impl Default for Transfer {
 impl Transfer {
     pub fn ft_on_transfer(
         &mut self,
-        account_id: AccountId,
+        token_id: AccountId,
         amount: u128,
-        msg: String,
     ) -> PromiseOrValue<U128> {
-        if !self.is_metadata_correct(msg.clone()) {
-            log!("Something wrong with message, metadata not correct.");
-            PromiseOrValue::Value(0);
+        if self.available_tokens.contains(&token_id.clone().to_string()) {
+            return self.update_balance(token_id, amount);
         }
-        self.lock(account_id, msg);
-        PromiseOrValue::Value(U128::from(amount))
+        return PromiseOrValue::Value(U128::from(amount));
     }
 
     pub fn lock(
         &mut self,
-        account_id: AccountId,
         msg: String,
-    ) {
-        self.locked_accounts.insert(&account_id, &msg);
+    ) -> PromiseOrValue<U128> {
+        let mut transfer_message: TransferMessage;
+        match self.is_metadata_correct(msg.clone()) {
+            Ok(tm) => { transfer_message = tm }
+            Err(err) => {
+                log!("Metadata not correct: {}", err);
+                return PromiseOrValue::Value(U128::from(0));
+            }
+        }
+
+        let user_token_balance = self.user_balances.get(&env::signer_account_id()).unwrap();
+        if let token_tr_balance = user_token_balance.get(&transfer_message.transfer.token).unwrap() {
+            if token_tr_balance < transfer_message.transfer.amount {
+                log!("Not enough transfer token balance.");
+                //TODO: place for emit event
+                return PromiseOrValue::Value(U128::from(0));
+            }
+        }
+
+        if let token_fee_balance = user_token_balance.get(&transfer_message.fee.token).unwrap() {
+            if token_fee_balance < transfer_message.fee.amount {
+                log!("Not enough fee token balance.");
+                //TODO: place for emit event
+                return PromiseOrValue::Value(U128::from(0));
+            }
+        }
+
+        self.substract_balance(&transfer_message.transfer.token, &transfer_message.transfer.amount);
+        self.substract_balance(&transfer_message.fee.token, &transfer_message.fee.amount);
+
+        PromiseOrValue::Value(U128::from(self.store_transfers(transfer_message) as u128))
     }
 
     pub fn unlock(
         &mut self,
-        account_id:
-        AccountId,
+        nonce: u64,
+    ) -> PromiseOrValue<U128> {
+        let transaction_id = str::from_utf8(&sha256(&nonce.to_string().as_bytes())).unwrap();
+        if let transfer = self.pending_transfers.get(&transaction_id.clone().to_string()).unwrap() {
+            if let transfer_data = transfer.get(&signer_account_id()).unwrap() {
+                if block_timestamp() < transfer_data.valid_till {
+                    self.increase_balance(&transfer_data.transfer.token, &transfer_data.transfer.amount);
+                    self.increase_balance(&transfer_data.fee.token, &transfer_data.fee.amount);
+
+                    self.pending_transfers.remove(&transaction_id.to_string());
+
+                    return self.withdraw(transfer_data);
+                } else {
+                    panic!("Valid time is not correct.");
+                }
+            } else {
+                panic!("Signer not same.");
+            }
+        } else {
+            panic!("Transaction id not correct");
+        }
+    }
+
+    #[private]
+    pub fn update_balance(
+        &mut self,
+        token_id: AccountId,
+        amount: u128,
+    ) -> PromiseOrValue<U128> {
+        if let mut user_balances = self.user_balances.get(&signer_account_id()).unwrap() {
+            if let mut token_amount = user_balances.get(&token_id).unwrap() {
+                token_amount += amount;
+                user_balances.insert(&token_id, &token_amount);
+            } else {
+                user_balances.insert(&token_id, &amount);
+            }
+            self.user_balances.insert(&signer_account_id(), &user_balances);
+        } else {
+            let mut token_balance = LookupMap::new(b"tb".to_vec());
+            token_balance.insert(&token_id, &amount);
+            self.user_balances.insert(&signer_account_id(), &token_balance);
+        }
+        return PromiseOrValue::Value(U128::from(0));
+    }
+
+    #[private]
+    pub fn substract_balance(
+        &mut self,
+        token_id: &AccountId,
+        amount: &u128,
     ) {
-        self.locked_accounts.remove(&account_id);
+        let mut user_token_balance = self.user_balances.get(&env::signer_account_id()).unwrap();
+        let balance = user_token_balance.get(&token_id).unwrap() - amount;
+        user_token_balance.insert(&token_id, &balance);
+        self.user_balances.insert(&env::signer_account_id(), &user_token_balance);
     }
 
 
+    #[private]
+    pub fn increase_balance(
+        &mut self,
+        token_id: &AccountId,
+        amount: &u128,
+    ) {
+        let mut user_token_balance = self.user_balances.get(&env::signer_account_id()).unwrap();
+        let balance = user_token_balance.get(&token_id).unwrap() + amount;
+        user_token_balance.insert(&token_id, &balance);
+        self.user_balances.insert(&env::signer_account_id(), &user_token_balance);
+    }
+
+    #[private]
     pub fn is_metadata_correct(
         &mut self,
         msg: String,
-    ) -> bool {
-        let mut is_correct: bool = true;
-        if msg.is_empty() {
-            panic!("Token message is empty.");
-        }
-
-        let token_msg: TokenMsg = serde_json::from_str(&msg)
+    ) -> Result<TransferMessage, &'static str> {
+        let transfer_message: TransferMessage = serde_json::from_str(&msg)
             .expect("Some error with json structure.");
-        if token_msg.valid_till < block_timestamp() {
-            log!("Transfer valid time not correct.");
-            is_correct = false;
+        if transfer_message.valid_till < block_timestamp() {
+            return Err("Transfer valid time not correct.");
         }
 
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards.");
-        let current_timestamp = since_the_epoch.as_secs();
-
-        let lock_period = token_msg.valid_till - current_timestamp;
+        let lock_period = transfer_message.valid_till - block_timestamp();
         if lock_period > LOCK_TIME_MAX ||
             lock_period < LOCK_TIME_MIN {
-            log!("Lock period does not fit the terms of the contract.");
-            is_correct = false;
+            return Err("Lock period does not fit the terms of the contract.");
         }
 
-        if !self.available_tokens.contains(&token_msg.transfer.token) {
-            log!("This transfer token not available.");
-            is_correct = false;
+        if !self.available_tokens.contains(&transfer_message.transfer.token.to_string()) {
+            return Err("This transfer token not available.");
         }
 
-        if !self.available_tokens.contains(&token_msg.fee.token) {
-            log!("This fee token not available.");
-            is_correct = false;
+        if !self.available_tokens.contains(&transfer_message.fee.token.to_string()) {
+            return Err("This fee token not available.");
         }
-        is_correct
+
+        return Ok(transfer_message);
     }
 
+    #[private]
     pub fn add_available_token(
         &mut self,
         token: String,
     ) {
         self.available_tokens.insert(&token);
+    }
+
+    #[private]
+    pub fn store_transfers(
+        &mut self,
+        transfer_message: TransferMessage,
+    ) -> u64 {
+        self.transactions += 1;
+        let transaction_id = str::from_utf8(&sha256(&self.transactions.to_string().as_bytes())).unwrap();
+        let mut account_pending = LookupMap::new(b"pt".to_vec());
+        account_pending.insert(&signer_account_id(), &transfer_message);
+        self.pending_transfers.insert(&transaction_id.to_string(), &account_pending);
+        self.transactions
+    }
+
+    #[private]
+    pub fn withdraw(
+        &mut self,
+        transfer_message: TransferMessage,
+    ) -> PromiseOrValue<U128> {
+        return ext_token::ft_transfer(
+            &transfer_message.transfer.token,
+            &transfer_message.transfer.amount,
+            NO_DEPOSIT,
+            TGAS,
+        ).then(ext_self::withdraw_amount_callback(
+            transfer_message,
+            NO_DEPOSIT,
+            self.terra_gas(40))).into();
+    }
+
+    #[allow(dead_code)]
+    pub fn withdraw_amount_callback(
+        &mut self,
+        transfer_message: TransferMessage,
+    ) -> PromiseOrValue<U128> {
+        assert_eq!(
+            env::promise_results_count(),
+            1,
+            "Withdraw amount callback."
+        );
+
+        return ext_token::ft_transfer(
+            &transfer_message.fee.token,
+            &transfer_message.fee.amount,
+            NO_DEPOSIT,
+            self.terra_gas(5),
+        ).then(ext_self::update_balance_callback(
+            transfer_message,
+            NO_DEPOSIT,
+            self.terra_gas(5),
+        )).into();
+    }
+
+    #[allow(dead_code)]
+    pub fn update_balance_callback(
+        &mut self,
+        transfer_message: TransferMessage,
+    ) -> PromiseOrValue<U128> {
+        assert_eq!(
+            env::promise_results_count(),
+            1,
+            "Update balance callback method."
+        );
+
+        self.substract_balance(&transfer_message.transfer.token, &transfer_message.transfer.amount);
+        self.substract_balance(&transfer_message.fee.token, &transfer_message.fee.amount);
+
+        //TODO: emit event
+        return PromiseOrValue::Value(U128::from(0));
+    }
+
+    #[private]
+    pub fn terra_gas(&self, gas: u64) -> Gas {
+        TGAS * gas
     }
 }
 
@@ -151,7 +323,7 @@ mod tests {
         assert!(contract.available_tokens.contains(&test_token));
     }
 
-    #[test]
+    /*#[test]
     fn is_metadata_correct_test() {
         let context = get_context(false);
         testing_env!(context);
@@ -163,12 +335,7 @@ mod tests {
         assert!(contract.available_tokens.contains(&test_token));
         assert!(contract.available_tokens.contains(&test_token2));
 
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards.");
-        // Time suits the conditions
-        let current_timestamp = since_the_epoch.as_secs() + LOCK_TIME_MIN + 20;
+        let current_timestamp = block_timestamp() + LOCK_TIME_MIN + 20;
         let mut msg: String = r#"
         {
             "valid_till": "#.to_owned();
@@ -196,12 +363,7 @@ mod tests {
         contract.add_available_token(test_token.clone());
         assert!(contract.available_tokens.contains(&test_token));
 
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards.");
-        // Time not suits the conditions
-        let current_timestamp = since_the_epoch.as_secs();
+        let current_timestamp = block_timestamp();
         let mut msg: String = r#"
         {
             "valid_till": "#.to_owned();
@@ -232,12 +394,8 @@ mod tests {
         assert!(contract.available_tokens.contains(&test_token));
         assert!(contract.available_tokens.contains(&test_token2));
 
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards.");
-        // Time suits the conditions
-        let current_timestamp = since_the_epoch.as_secs() + LOCK_TIME_MIN + 20;
+
+        let current_timestamp = block_timestamp() + LOCK_TIME_MIN + 20;
         let mut msg: String = r#"
         {
             "valid_till": "#.to_owned();
@@ -254,7 +412,7 @@ mod tests {
             "recipient": "RESJDHF34JDHRYUIKKO83UNMNX2"
         }"#);
         assert!(contract.is_metadata_correct(String::from(msg.clone())));
-        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap(), msg);
+        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap(), 200, msg);
         assert!(contract.locked_accounts.contains_key(&AccountId::try_from("alice_near".to_string()).unwrap()));
     }
 
@@ -278,12 +436,9 @@ mod tests {
         assert!(contract.available_tokens.contains(&test_token));
         assert!(contract.available_tokens.contains(&test_token2));
 
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards.");
+
         // Time suits the conditions
-        let current_timestamp = since_the_epoch.as_secs() + LOCK_TIME_MIN + 20;
+        let current_timestamp = block_timestamp() + LOCK_TIME_MIN + 20;
         let mut msg: String = r#"
         {
             "valid_till": "#.to_owned();
@@ -300,9 +455,9 @@ mod tests {
             "recipient": "RESJDHF34JDHRYUIKKO83UNMNX2"
         }"#);
         assert!(contract.is_metadata_correct(String::from(msg.clone())));
-        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap(), msg);
+        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap(), 200, msg);
         assert!(contract.locked_accounts.contains_key(&AccountId::try_from("alice_near".to_string()).unwrap()));
         contract.unlock(AccountId::try_from("alice_near".to_string()).unwrap());
         assert!(!contract.locked_accounts.contains_key(&AccountId::try_from("alice_near".to_string()).unwrap()));
-    }
+    }*/
 }
