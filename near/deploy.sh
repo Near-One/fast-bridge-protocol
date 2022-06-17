1b429ea3924ca71dbaff935fbd71243f15ee0a79
tymofii1991 <91468680+TymofiiSavinov@users.noreply.github.com>
2022-06-17 11:00:39 +0300

    PR: Latest deploy script (#22)
    * latest deploy script
    
    * comments added
    
    Co-authored-by: Tymofii <timofii.savinov@gmail.com>

--- near/deploy.sh
+++ near/deploy.sh
@@ -9,7 +9,7 @@ near delete transfer.spectrebridge2.testnet spectrebridge2.testnet
 near delete token.spectrebridge2.testnet spectrebridge2.testnet
 
 # create corresponding accoutns
-near create-account transfer.spectrebridge2.testnet --masterAccount spectrebridge2.testnet --initialBalance 10
+near create-account transfer.spectrebridge2.testnet --masterAccount spectrebridge2.testnet --initialBalance 20
 near create-account token.spectrebridge2.testnet --masterAccount spectrebridge2.testnet --initialBalance 10
 
 # redeploy contracts
@@ -20,7 +20,6 @@ near deploy token.spectrebridge2.testnet --wasmFile ./res/mock_token.wasm --init
 near call token.spectrebridge2.testnet mint '{"account_id": "transfer.spectrebridge2.testnet", "amount": "1000000000"}' --accountId spectrebridge2.testnet
 
 # add mock data
-near call transfer.spectrebridge2.testnet add_supported_token '{"token": "token.spectrebridge2.testnet"}'  --account-id transfer.spectrebridge2.testnet
 near call transfer.spectrebridge2.testnet ft_on_transfer '{"token_id": "token.spectrebridge2.testnet", "amount": 100}' --account-id transfer.spectrebridge2.testnet
-near call transfer.spectrebridge2.testnet lock '{"msg": "{\"valid_till\":1652038871250000000,\"transfer\":{\"token\":\"token.spectrebridge2.testnet\",\"amount\":50},\"fee\":{\"token\":\"token.spectrebridge2.testnet\",\"amount\":50},\"recipient\":\"00005474e89094c44da98b954eedeac495271d0f\"}"}' --account-id transfer.spectrebridge2.testnet
-#near call transfer.spectrebridge2.testnet lp_unlock '{}' --account-id transfer.spectrebridge2.testnet --gas 300000000000000
+# valid_till must be current timestamp + min time lock in nanoseconds
+near call transfer.spectrebridge2.testnet lock '{"msg": "{\"chain_id\":5, \"valid_till\":1655321232634650000,\"transfer\":{\"token_near\":\"token.spectrebridge2.testnet\",\"token_eth\":[113, 199, 101, 110, 199, 171, 136, 176, 152, 222, 251, 117, 27, 116, 1, 181, 246, 216, 151, 111],\"amount\":\"50\"},\"fee\":{\"token\":\"token.spectrebridge2.testnet\",\"amount\":\"50\"},\"recipient\":[113, 199, 101, 110, 199, 171, 136, 176, 152, 222, 251, 117, 27, 116, 1, 181, 246, 216, 151, 111]}"}' --account-id transfer.spectrebridge2.testnet --gas 300000000000000
