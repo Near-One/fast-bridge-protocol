096af4cd6aa853aaa3f11d8d4d586125850e8d5f
annaBlaize <89964361+annaBlaize@users.noreply.github.com>
2022-08-30 18:48:23 +0300

    feat: update hardhat tools and config

--- eth/hardhat.config.js
+++ eth/hardhat.config.js
@@ -1,35 +1,19 @@
 require("dotenv").config();
 
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+require("@nomicfoundation/hardhat-toolbox");
+
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-network-helpers");
+
 require("hardhat-contract-sizer");
 require("hardhat-abi-exporter");
 require("@openzeppelin/hardhat-upgrades");
-require("@nomiclabs/hardhat-web3");
 
-// See `README.md` for details
-
-/*
- * Private keys for the network configuration.
- *
- * Setting in `.env` file.
- */
 const PRIVATE_KEYS = process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(",") : [];
+const ENABLED_OPTIMIZER = true;
+const OPTIMIZER_RUNS = 200; 
 
-/*
- * The solc compiler optimizer configuration. (The optimizer is disabled by default).
- *
- * Set `ENABLED_OPTIMIZER` in `.env` file to true for enabling.
- */
-// `!!` to convert to boolean
-const ENABLED_OPTIMIZER = !!process.env.ENABLED_OPTIMIZER || !!process.env.REPORT_GAS || false;
-const OPTIMIZER_RUNS = process.env.OPTIMIZER_RUNS ? +process.env.OPTIMIZER_RUNS : 200; // `+` to convert to number
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
     solidity: {
         compilers: [
@@ -41,25 +25,9 @@ module.exports = {
                         runs: OPTIMIZER_RUNS
                     }
                 }
-            }//,
-            // { // Example of adding multiple compiler versions within the same project
-            //     version: "0.7.6",
-            //     settings: {
-            //         optimizer: {
-            //             enabled: ENABLED_OPTIMIZER,
-            //             runs: OPTIMIZER_RUNS
-            //         }
-            //     }
-            // }
-        ]//,
-        // overrides: { // Example of specifying a compiler for a specified contract
-        //     "contracts/Foo.sol": {
-        //         version: "0.5.5",
-        //         settings: { }
-        //     }
-        // }
+            }
+        ]
     },
-    // defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             allowUnlimitedContractSize: !ENABLED_OPTIMIZER,
@@ -68,7 +36,6 @@ module.exports = {
                 enabled: process.env.FORKING !== undefined
             }
         },
-        // Rest parameter (...) to treat it as a single array (added in ES6)
         mainnet: {
             url: process.env.MAINNET_URL || "",
             accounts: [...PRIVATE_KEYS]
@@ -88,29 +55,12 @@ module.exports = {
     },
     gasReporter: {
         enabled: process.env.REPORT_GAS !== undefined,
-        // excludeContracts: ["mocks/"],
         currency: "USD",
         outputFile: process.env.GAS_REPORT_TO_FILE ? "gas-report.txt" : undefined
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
         url: process.env.ETHERSCAN_URL || ""
-        // Can be done this way for using of multiple API keys
-        // This is not necessarily the same name that is used to define the network. See the link for details:
-        // https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html#multiple-api-keys-and-alternative-block-explorers
-        //
-        // apiKey: {
-        //     mainnet: "YOUR_ETHERSCAN_API_KEY",
-        //     rinkeby: "YOUR_ETHERSCAN_API_KEY",
-        //     ropsten: "YOUR_ETHERSCAN_API_KEY",
-        //     kovan: "YOUR_ETHERSCAN_API_KEY"
-        // }//,
-        // url: {
-        //     mainnet: "ETHERSCAN_URL",
-        //     rinkeby: "ETHERSCAN_URL",
-        //     ropsten: "ETHERSCAN_URL",
-        //     kovan: "ETHERSCAN_URL"
-        // }
     },
     contractSizer: {
         except: ["mocks/"]
@@ -121,14 +71,8 @@ module.exports = {
     }
 };
 
-// By default fork from the latest block
 if (process.env.FORKING_BLOCK_NUMBER)
     module.exports.networks.hardhat.forking.blockNumber = +process.env.FORKING_BLOCK_NUMBER;
 
-/*
- * This setting changes how Hardhat Network works, to mimic Ethereum's mainnet at a given hardfork. It must be one of
- * "byzantium", "constantinople", "petersburg", "istanbul", "muirGlacier", "berlin", "london" and "arrowGlacier".
- * Default value: "arrowGlacier".
- */
 if (process.env.HARDFORK)
     module.exports.networks.hardhat.hardfork = process.env.HARDFORK;
