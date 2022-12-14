9aebd763a8447c951db7f90fd1a6a8d01bfac788
karim-en <karim@aurora.dev>
2022-12-14 00:05:43 +0000

    Remove submodule & update dependences

--- near/build.sh
+++ near/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo build --manifest-path ./contracts/Cargo.toml --target wasm32-unknown-unknown --release
-cp ./**/target/wasm32-unknown-unknown/release/*.wasm ./res/
+mkdir -p res
+cd contracts
+RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+cp ./target/wasm32-unknown-unknown/release/*.wasm ../res/
 
