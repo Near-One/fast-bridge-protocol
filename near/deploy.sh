d43d17047f3a52926a87de93f8bf6b189760bfd0
karim-en <karim@aurora.dev>
2022-12-14 03:56:11 +0000

    Improve deploy script

--- near/deploy.sh
+++ near/deploy.sh
@@ -4,27 +4,38 @@
 # build & test
 ./build.sh && ./test.sh
 
+MASTER_ACCOUNT="<YOUR_ACCOUNT>"
+BRIDGE_ACCOUNT=transfer.$MASTER_ACCOUNT
+TOKEN_ACCOUNT=token.$MASTER_ACCOUNT
+MIN_TIME_LOCK_NS=3600000000000
+
 # clean up previuos deployment
-near delete transfer.spectrebridge.testnet spectrebridge.testnet
-near delete token.spectrebridge.testnet spectrebridge.testnet
+near delete $BRIDGE_ACCOUNT $MASTER_ACCOUNT
+near delete $TOKEN_ACCOUNT $MASTER_ACCOUNT
 
 # create corresponding accoutns
-near create-account transfer.spectrebridge.testnet --masterAccount spectrebridge.testnet --initialBalance 20
-near create-account token.spectrebridge.testnet --masterAccount spectrebridge.testnet --initialBalance 10
+near create-account $BRIDGE_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBalance 20
+near create-account $TOKEN_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBalance 10
 
 # redeploy contracts
-near deploy transfer.spectrebridge.testnet --wasmFile ./res/bridge.wasm
-near deploy token.spectrebridge.testnet --wasmFile ./res/mock_token.wasm --initFunction 'new_default_meta' --initArgs '{"owner_id": "spectrebridge.testnet", "name": "Wrapped Near", "symbol": "WNEAR", "total_supply": "1"}'
+near deploy $BRIDGE_ACCOUNT --wasmFile ./res/bridge.wasm
+near deploy $TOKEN_ACCOUNT --wasmFile ./res/mock_token.wasm --initFunction 'new_default_meta' --initArgs '{"owner_id": "'"$MASTER_ACCOUNT"'", "name": "Wrapped Near", "symbol": "WNEAR", "total_supply": "1"}'
 
 
-near call token.spectrebridge.testnet mint '{"account_id": "spectrebridge.testnet", "amount": "1000000000000000000000000000"}' --accountId spectrebridge.testnet
+near call $TOKEN_ACCOUNT mint '{"account_id": "'"$MASTER_ACCOUNT"'", "amount": "1000000000000000000000000000"}' --accountId $MASTER_ACCOUNT
 
-# fund transfer.spectrebridge.testnet
-near call token.spectrebridge.testnet storage_deposit '{"account_id": "transfer.spectrebridge.testnet"}' --accountId spectrebridge.testnet --amount 0.25
+# fund $BRIDGE_ACCOUNT
+near call $TOKEN_ACCOUNT storage_deposit '{"account_id": "'"$BRIDGE_ACCOUNT"'"}' --accountId $MASTER_ACCOUNT --amount 0.25
 
 # add mock data
-# near call transfer.spectrebridge.testnet ft_on_transfer '{"token_id": "token.spectrebridge.testnet", "amount": 10000000000000000000000000}' --account-id spectrebridge.testnet
-near call token.spectrebridge.testnet ft_transfer_call '{"receiver_id": "transfer.spectrebridge.testnet", "amount": "10000000000000000000000000", "msg": ""}' --account-id spectrebridge.testnet --depositYocto 1 --gas 300000000000000
+# near call $BRIDGE_ACCOUNT ft_on_transfer '{"token_id": "'"$TOKEN_ACCOUNT"'", "amount": 10000000000000000000000000}' --account-id $MASTER_ACCOUNT
+near call $TOKEN_ACCOUNT ft_transfer_call '{"receiver_id": "'"$BRIDGE_ACCOUNT"'", "amount": "10000000000000000000000000", "msg": ""}' --account-id $MASTER_ACCOUNT --depositYocto 1 --gas 300000000000000
 
-# valid_till must be current timestamp + min time lock in nanoseconds
-near call transfer.spectrebridge.testnet lock '{"msg": "{\"chain_id\":5, \"valid_till\": 1656491326879389137,\"transfer\":{\"token_near\":\"token.spectrebridge.testnet\",\"token_eth\":[ 178,  215,  92,  90,  20,  42,  104,  189,  164,  56,  230,  163,  24,  199,  251,  178,  36,  47,  150,  147 ],\"amount\":\"9000000000000000000000000\"},\"fee\":{\"token\":\"token.spectrebridge.testnet\",\"amount\":\"1000000000000000000000000\"},\"recipient\":[ 42,  35,  224,  250,  58,  254,  119,  175,  245,  220,  108,  106,  0,  126,  58,  16,  193,  69,  6,  51 ]}"}' --account-id spectrebridge.testnet --gas 300000000000000
+near view $BRIDGE_ACCOUNT get_lock_duration
+
+sec_to_ns=1000000000
+current_timestamp=$(($(date +%s)*$sec_to_ns))
+valid_till=$((current_timestamp + MIN_TIME_LOCK_NS + 15000000000))
+
+# valid_till is current timestamp + min time lock in nanoseconds + extra 15 sec
+near call $BRIDGE_ACCOUNT init_transfer "{\"transfer_message\":{\"chain_id\":5,\"valid_till\":"$valid_till",\"transfer\":{\"token_near\":"\"$TOKEN_ACCOUNT\"",\"token_eth\":[178,215,92,90,20,42,104,189,164,56,230,163,24,199,251,178,36,47,150,147],\"amount\":\"9000000000000000000000000\"},\"fee\":{\"token\":"\"$TOKEN_ACCOUNT\"",\"amount\":\"1000000000000000000000000\"},\"recipient\":[42,35,224,250,58,254,119,175,245,220,108,106,0,126,58,16,193,69,6,51]}}" --account-id $MASTER_ACCOUNT --gas 300000000000000
