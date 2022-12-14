1c933f64d32ba340af191519832255ae39832b5b
karim-en <karim@aurora.dev>
2022-12-14 21:24:10 +0000

    Add target in build script

--- near/build.sh
+++ near/build.sh
@@ -3,6 +3,7 @@ set -e
 
 mkdir -p res
 cd contracts
+rustup target add wasm32-unknown-unknown
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp ./target/wasm32-unknown-unknown/release/*.wasm ../res/
 
