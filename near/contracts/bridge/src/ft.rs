692a00051b5ecb8d37e4ac57d5b865304ac890fb
karim-en <karim@aurora.dev>
2023-01-03 00:06:00 +0000

    Init transfer on `ft_on_transfer`

--- near/contracts/bridge/src/ft.rs
+++ near/contracts/bridge/src/ft.rs
@@ -1,7 +1,7 @@
 use crate::*;
 
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
-use near_sdk::AccountId;
+use near_sdk::{serde_json, AccountId};
 
 #[near_bindgen]
 impl FungibleTokenReceiver for SpectreBridge {
@@ -9,7 +9,7 @@ impl FungibleTokenReceiver for SpectreBridge {
         &mut self,
         sender_id: AccountId,
         amount: U128,
-        #[allow(unused_variables)] msg: String,
+        msg: String,
     ) -> PromiseOrValue<U128> {
         require!(
             sender_id == env::signer_account_id(),
@@ -18,6 +18,14 @@ impl FungibleTokenReceiver for SpectreBridge {
 
         let token_account_id = env::predecessor_account_id();
         self.check_whitelist_token_and_account(&token_account_id, &sender_id);
-        self.update_balance(sender_id, token_account_id, amount.0)
+        self.update_balance(sender_id.clone(), token_account_id, amount.0);
+
+        if !msg.is_empty() {
+            let transfer_message: TransferMessage = serde_json::from_str(&msg)
+                .unwrap_or_else(|_| env::panic_str("Invalid json format of the `TransferMessage`"));
+            self.init_transfer_internal(transfer_message, sender_id);
+        }
+
+        PromiseOrValue::Value(U128::from(0))
     }
 }
