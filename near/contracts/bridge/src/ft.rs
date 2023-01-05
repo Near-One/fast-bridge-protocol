08ba167e2506ad69c77b473b6759fa50021ea587
karim-en <karim@aurora.dev>
2023-01-05 03:41:45 +0000

    Refund tokens if `init_transfer` fail

--- near/contracts/bridge/src/ft.rs
+++ near/contracts/bridge/src/ft.rs
@@ -19,12 +19,27 @@ impl FungibleTokenReceiver for SpectreBridge {
 
         let token_account_id = env::predecessor_account_id();
         self.check_whitelist_token_and_account(&token_account_id, &sender_id);
-        self.update_balance(sender_id.clone(), token_account_id, amount.0);
 
         if !msg.is_empty() {
             let transfer_message: TransferMessage = serde_json::from_str(&msg)
                 .unwrap_or_else(|_| env::panic_str("Invalid json format of the `TransferMessage`"));
-            self.init_transfer_internal(transfer_message, sender_id);
+
+            let update_balance = UpdateBalance {
+                sender_id: sender_id.clone(),
+                token: token_account_id,
+                amount,
+            };
+
+            return self
+                .init_transfer_internal(transfer_message, sender_id, Some(update_balance))
+                .then(
+                    ext_self::ext(env::current_account_id())
+                        .with_static_gas(utils::tera_gas(5))
+                        .init_transfer_internal_callback(),
+                )
+                .into();
+        } else {
+            self.update_balance(sender_id.clone(), token_account_id.clone(), amount.0);
         }
 
         PromiseOrValue::Value(U128::from(0))
--- near/contracts/bridge/src/lib.rs
+++ near/contracts/bridge/src/lib.rs
@@ -67,7 +67,17 @@ trait SpectreBridgeInterface {
         &mut self,
         #[serializer(borsh)] transfer_message: TransferMessage,
         #[serializer(borsh)] sender_id: AccountId,
+        #[serializer(borsh)] update_balance: Option<UpdateBalance>,
     ) -> PromiseOrValue<U128>;
+    fn init_transfer_internal_callback(&mut self) -> U128;
+}
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Debug, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct UpdateBalance {
+    sender_id: AccountId,
+    token: AccountId,
+    amount: U128,
 }
 
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize, Debug, Clone)]
@@ -184,23 +194,40 @@ impl SpectreBridge {
 
     #[pause]
     pub fn init_transfer(&mut self, transfer_message: TransferMessage) -> PromiseOrValue<U128> {
-        self.init_transfer_internal(transfer_message, env::predecessor_account_id())
+        self.init_transfer_internal(transfer_message, env::predecessor_account_id(), None)
+            .into()
     }
 
     fn init_transfer_internal(
         &mut self,
         transfer_message: TransferMessage,
         sender_id: AccountId,
-    ) -> PromiseOrValue<U128> {
+        update_balance: Option<UpdateBalance>,
+    ) -> Promise {
         ext_eth_client::ext(self.eth_client_account.clone())
             .with_static_gas(utils::tera_gas(5))
             .last_block_number()
             .then(
                 ext_self::ext(env::current_account_id())
                     .with_static_gas(utils::tera_gas(200))
-                    .init_transfer_callback(transfer_message, sender_id),
+                    .init_transfer_callback(transfer_message, sender_id, update_balance),
             )
-            .into()
+    }
+
+    #[private]
+    pub fn init_transfer_internal_callback(&self) -> U128 {
+        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
+        match env::promise_result(0) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Successful(val) => {
+                if let Ok(_nonce) = near_sdk::serde_json::from_slice::<U128>(&val) {
+                    return U128::from(0);
+                } else {
+                    env::panic_str("ERR_WRONG_VAL_RECEIVED")
+                }
+            }
+            PromiseResult::Failed => env::panic_str("ERR_CALL_FAILED"),
+        }
     }
 
     #[private]
@@ -211,7 +238,16 @@ impl SpectreBridge {
         last_block_height: u64,
         #[serializer(borsh)] transfer_message: TransferMessage,
         #[serializer(borsh)] sender_id: AccountId,
-    ) -> PromiseOrValue<U128> {
+        #[serializer(borsh)] update_balance: Option<UpdateBalance>,
+    ) -> U128 {
+        if let Some(update_balance) = update_balance {
+            self.update_balance(
+                update_balance.sender_id,
+                update_balance.token,
+                update_balance.amount.0,
+            );
+        }
+
         let mut transfer_message = transfer_message;
         let lock_period = transfer_message.valid_till - block_timestamp();
         transfer_message.valid_till_block_height =
@@ -285,7 +321,7 @@ impl SpectreBridge {
         }
         .emit();
 
-        PromiseOrValue::Value(nonce)
+        nonce
     }
 
     #[pause(except(roles(Role::UnrestrictedUnlock)))]
