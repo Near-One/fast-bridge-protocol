4ec0265947c9c1ba114a303573588bbb918fb6c1
karim-en <karim@aurora.dev>
2023-01-10 15:44:24 +0000

    Improve deploy script

--- near/deploy.sh
+++ near/deploy.sh
@@ -8,6 +8,7 @@ MASTER_ACCOUNT="<YOUR_ACCOUNT>"
 BRIDGE_ACCOUNT=fast-bridge.$MASTER_ACCOUNT
 TOKEN_ACCOUNT=token.$MASTER_ACCOUNT
 MIN_TIME_LOCK_NS=3600000000000
+NEAR_ENV=testnet
 
 # clean up previuos deployment
 near delete $BRIDGE_ACCOUNT $MASTER_ACCOUNT
@@ -21,14 +22,17 @@ near create-account $TOKEN_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBala
 near deploy $BRIDGE_ACCOUNT --wasmFile ./res/bridge.wasm --initGas   300000000000000 --initFunction 'new' --initArgs '{"eth_bridge_contract": "23244a6c91e66526e4a0959B2457a702aE661Acf", "prover_account": "prover.goerli.testnet", "eth_client_account": "client-eth2.goerli.testnet", "lock_time_min": "1h", "lock_time_max": "24h", "eth_block_time": 12000000000}'
 near deploy $TOKEN_ACCOUNT --wasmFile ./res/mock_token.wasm --initFunction 'new_default_meta' --initArgs '{"owner_id": "'"$MASTER_ACCOUNT"'", "name": "Wrapped Near", "symbol": "WNEAR", "total_supply": "1"}'
 
+# grant roles
+near call $BRIDGE_ACCOUNT acl_grant_role '{"role": "WhitelistManager", "account_id": "'"$MASTER_ACCOUNT"'"}' --accountId $BRIDGE_ACCOUNT
+near call $BRIDGE_ACCOUNT acl_grant_role '{"role": "ConfigManager", "account_id": "'"$MASTER_ACCOUNT"'"}' --accountId $BRIDGE_ACCOUNT
+
+# add the token to whitelist
+near call $BRIDGE_ACCOUNT set_token_whitelist_mode '{"token": "'"$TOKEN_ACCOUNT"'", "mode": "CheckToken"}' --accountId $MASTER_ACCOUNT
 
 near call $TOKEN_ACCOUNT mint '{"account_id": "'"$MASTER_ACCOUNT"'", "amount": "1000000000000000000000000000"}' --accountId $MASTER_ACCOUNT
 
 # fund $BRIDGE_ACCOUNT
 near call $TOKEN_ACCOUNT storage_deposit '{"account_id": "'"$BRIDGE_ACCOUNT"'"}' --accountId $MASTER_ACCOUNT --amount 0.25
-
-# add the token to whitelist
-near call $BRIDGE_ACCOUNT set_token_whitelist_mode '{"token": "'"$TOKEN_ACCOUNT"'", "mode": "CheckToken"}' --accountId $BRIDGE_ACCOUNT
 
 # add mock data
 # near call $BRIDGE_ACCOUNT ft_on_transfer '{"token_id": "'"$TOKEN_ACCOUNT"'", "amount": 10000000000000000000000000}' --account-id $MASTER_ACCOUNT
