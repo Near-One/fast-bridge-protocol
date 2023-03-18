8d869354e953c3aa85994ec2e92a8204d61fdb41
UrAvgDeveloper <adi.b20612@gmail.com>
2023-03-18 17:37:08 +0530

    changed deployment script for localnet test

--- eth/scripts/deployment/deploy_bridge_ETH.js
+++ eth/scripts/deployment/deploy_bridge_ETH.js
@@ -2,10 +2,12 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
-    const tokensAddresses = Object.values(require("./deploymentAdd.json").tokens);
-    const whitelistedTokens = Object.values(require("./deploymentAdd.json").whitelisted_tokens);
+    const Token = await ethers.getContractFactory("TestToken");
+    const tokenDeployed = await Token.deploy(18, "ETH", "ETH");
+    await tokenDeployed.deployed();
+
     const bridge = (await ethers.getContractFactory("EthErc20FastBridge")).connect(deployer);
-    let proxy = await upgrades.deployProxy(bridge, [tokensAddresses, whitelistedTokens], {
+    let proxy = await upgrades.deployProxy(bridge, [[tokenDeployed.address], [true]], {
         unsafeAllow: ["delegatecall"]
     });
     await proxy.deployed();
@@ -15,6 +17,7 @@ async function main() {
     console.log("Proxy FastBridge Deployed at: ", proxy.address);
     console.log("FastBridge Implemenation Address: ", bridge_implementation_address);
     console.log("Proxy FastBridge Deployed by address: ", deployer.address);
+    console.log("Token Address: ", tokenDeployed.address)
 }
 
 main().catch((error) => {
