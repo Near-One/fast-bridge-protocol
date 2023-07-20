445d76810434a36a69febcd128c5267ec6a070b9
karim-en <karim@aurora.dev>
2023-07-20 21:59:45 +0100

    Upgrade rust to `1.71.0` for expand

--- near/expand.sh
+++ near/expand.sh
@@ -1,5 +1,5 @@
-RUST_VERSION=1.69.0
+RUST_VERSION=1.71.0
 rustup install $RUST_VERSION
-cargo +$RUST_VERSION install cargo-expand --version 1.0.56
+cargo +$RUST_VERSION install cargo-expand
 rustup target add wasm32-unknown-unknown --toolchain $RUST_VERSION
 cargo +$RUST_VERSION expand --manifest-path ./contracts/bridge/Cargo.toml --target wasm32-unknown-unknown > res/fastbridge_expand.rs
