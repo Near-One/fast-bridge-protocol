3c26699f253c7fa8b8c9d4d7b95358a028ade994
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2023-08-18 14:18:01 +0300

    update utils for using config

--- aurora/hardhat.config.js
+++ aurora/hardhat.config.js
@@ -44,46 +44,66 @@ task("upgrade", "Upgrade aurora fast bridge proxy contract")
     });
 
 task('tokens_registration', 'Register tokens and storage deposit on NEAR for the contract')
+    .addParam("silo", "Config file name without extension")
     .addParam('fastBridgeAddress', 'Aurora Fast Bridge address')
     .addParam('nearTokenAddress', "Token address on Near")
     .addParam('auroraTokenAddress', "Token address on Aurora")
     .setAction(async taskArgs => {
         const { tokensRegistration } = require('./scripts/utils');
-        await tokensRegistration(hre.ethers.provider, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress, taskArgs.auroraTokenAddress);
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await tokensRegistration(signer, config, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress, taskArgs.auroraTokenAddress);
     });
 
 task('init_token_transfer', 'Initialize Token Transfer from Aurora to Ethereum')
+    .addParam("silo", "Config file name without extension")
     .addParam('fastBridgeAddress', 'Aurora Fast Bridge address')
     .addParam('initTokenTransferArg', 'argument for token transfer initialization')
     .addParam('auroraTokenAddress', "Token address on Aurora")
     .setAction(async taskArgs => {
         const { initTokenTransfer } = require('./scripts/utils');
-        await initTokenTransfer(hre.ethers.provider, taskArgs.fastBridgeAddress, taskArgs.initTokenTransferArg, taskArgs.auroraTokenAddress);
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await initTokenTransfer(signer, config, taskArgs.fastBridgeAddress, taskArgs.initTokenTransferArg, taskArgs.auroraTokenAddress);
     });
 
 task('unlock', 'Unlock tokens on Near')
+    .addParam("silo", "Config file name without extension")
     .addParam('fastBridgeAddress', 'Aurora Fast Bridge address')
     .addParam('nonce', 'nonce')
     .setAction(async taskArgs => {
         const { unlock } = require('./scripts/utils');
-        await unlock(hre.ethers.provider, taskArgs.fastBridgeAddress, taskArgs.nonce);
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await unlock(signer, config, taskArgs.fastBridgeAddress, taskArgs.nonce);
     });
 
 task('withdraw_from_near', 'Withdraw tokens on Near side')
+    .addParam("silo", "Config file name without extension")
     .addParam('fastBridgeAddress', 'Aurora Fast Bridge address')
     .addParam('nearTokenAddress', "Token address on Near")
     .addParam('tokenAmount', "Withdraw tokens amount")
     .setAction(async taskArgs => {
         const { withdraw_from_near } = require('./scripts/utils');
-        await withdraw_from_near(hre.ethers.provider, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress, taskArgs.tokenAmount);
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await withdraw_from_near(signer, config, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress, taskArgs.tokenAmount);
     });
 
 task('withdraw', 'Withdraw tokens to user on Aurora')
+    .addParam("silo", "Config file name without extension")
     .addParam('fastBridgeAddress', 'Aurora Fast Bridge address')
     .addParam('nearTokenAddress', "Token address on Near")
     .setAction(async taskArgs => {
         const { withdraw } = require('./scripts/utils');
-        await withdraw(hre.ethers.provider, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress);
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await withdraw(signer, config, taskArgs.fastBridgeAddress, taskArgs.nearTokenAddress);
     });
 
 module.exports = {
@@ -116,4 +136,3 @@ module.exports = {
         timeout: 1000000000000
     }
 };
-
--- aurora/scripts/utils.js
+++ aurora/scripts/utils.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const hre = require("hardhat");
 
-async function tokensRegistration(provider, fastBridgeAddress, nearTokenAddress, auroraTokenAddress) {
-    const fastBridge = await beforeWorkWithFastBridge(provider, fastBridgeAddress);
+async function tokensRegistration(signer, config, fastBridgeAddress, nearTokenAddress, auroraTokenAddress) {
+    const fastBridge = await beforeWorkWithFastBridge(signer, config, fastBridgeAddress);
     await fastBridge.registerToken(auroraTokenAddress, nearTokenAddress);
 
     console.log("Aurora Fast Bridge Address on Near: ", await fastBridge.getNearAddress());
 }
 
-async function initTokenTransfer(provider, fastBridgeAddress, initTokenTransferArg, auroraTokenAddress) {
+async function initTokenTransfer(signer, config, fastBridgeAddress, initTokenTransferArg, auroraTokenAddress) {
     const usdc = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", auroraTokenAddress);
     await usdc.approve(fastBridgeAddress, "2000000000000000000000000");
 
-    const fastBridge = await beforeWorkWithFastBridge(provider, fastBridgeAddress);
+    const fastBridge = await beforeWorkWithFastBridge(signer, config, fastBridgeAddress);
 
     const options = { gasLimit: 5000000 };
     let tx = await fastBridge.initTokenTransfer(initTokenTransferArg, options);
@@ -20,45 +20,44 @@ async function initTokenTransfer(provider, fastBridgeAddress, initTokenTransferA
     console.log(receipt.events[0].args);
 }
 
-async function unlock(provider, fastBridgeAddress, nonce) {
-    const fastBridge = await beforeWorkWithFastBridge(provider, fastBridgeAddress);
+async function unlock(signer, config, fastBridgeAddress, nonce) {
+    const fastBridge = await beforeWorkWithFastBridge(signer, config, fastBridgeAddress);
 
     let tx = await fastBridge.unlock(nonce);
     let receipt = await tx.wait();
     console.log(receipt.events[0].args);
 }
 
-async function withdraw_from_near(provider, fastBridgeAddress, nearTokenAddress, amount) {
-    const fastBridge = await beforeWorkWithFastBridge(provider, fastBridgeAddress);
+async function withdraw_from_near(signer, config, fastBridgeAddress, nearTokenAddress, amount) {
+    const fastBridge = await beforeWorkWithFastBridge(signer, config, fastBridgeAddress);
 
     let tx = await fastBridge.withdrawFromNear(nearTokenAddress, amount);
     let receipt = await tx.wait();
 }
 
-async function withdraw(provider, fastBridgeAddress, nearTokenAddress) {
-    const fastBridge = await beforeWorkWithFastBridge(provider, fastBridgeAddress);
+async function withdraw(signer, config, fastBridgeAddress, nearTokenAddress) {
+    const fastBridge = await beforeWorkWithFastBridge(signer, config, fastBridgeAddress);
 
     let tx = await fastBridge.withdraw(nearTokenAddress);
     let receipt = await tx.wait();
 }
 
-async function beforeWorkWithFastBridge(provider, fastBridgeAddress) {
-    const deployerWallet = new hre.ethers.Wallet(process.env.AURORA_PRIVATE_KEY, provider);
-    console.log("Sending transaction with the account:", deployerWallet.address);
+async function beforeWorkWithFastBridge(signer, config, fastBridgeAddress) {
+    console.log("Sending transaction with the account:", signer.address);
 
-    const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", process.env.WNEAR_AURORA_ADDRESS);
+    const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
     await wnear.approve(fastBridgeAddress, "4012500000000000000000000");
 
     const FastBridge = await hre.ethers.getContractFactory("AuroraErc20FastBridge", {
         libraries: {
-            "AuroraSdk": process.env.AURORA_SDK_ADDRESS,
-            "Utils": process.env.AURORA_UTILS_ADDRESS
+            "AuroraSdk": config.auroraSdkAddress,
+            "Utils": config.auroraUtilsAddress
         },
     });
 
     return FastBridge
         .attach(fastBridgeAddress)
-        .connect(deployerWallet);
+        .connect(signer);
 }
 
 exports.initTokenTransfer = initTokenTransfer;
